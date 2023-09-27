7b43b9922b62cac9b7bdbb3def0fc402bfeeae47
metachoSamuel <samuel.f.met@gmail.com>
2023-09-27 00:45:42 -0500

    Configurando ambiente para el uso de Firebase

---
+++ src/environments/environment.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  firebase: {
+    projectId: 'vetcore-71c9d',
+    appId: '1:388812377494:web:5eb2230b41af151c0ba1b7',
+    storageBucket: 'vetcore-71c9d.appspot.com',
+    apiKey: 'AIzaSyCRnn3CR9Phz4Phlt4Io0LdExQI0tKVLds',
+    authDomain: 'vetcore-71c9d.firebaseapp.com',
+    messagingSenderId: '388812377494',
+  },
+};
