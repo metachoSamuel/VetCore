95c545757b304a624c7cfe870ec6b5d20bffcceb
metachoSamuel <samuel.f.met@gmail.com>
2023-10-13 20:30:18 -0500

    Avances en inicio de sesion y cerrar sesion

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -5,73 +5,73 @@ import {FormControl, FormGroup} from "@angular/forms";
 import Swal from 'sweetalert2';
 
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+    selector: 'app-login',
+    templateUrl: './login.component.html',
+    styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-  formlogin: FormGroup;
-  constructor(
-      private router: Router,
-      private userService: UserService
-  ) {
-    this.formlogin = new FormGroup({
-      email: new FormControl(),
-      password: new FormControl()
-    })
-  }
-
-  ngOnInit(): void {
-  }
-
-  navigateToRegistro() {
-    this.router.navigate(['/register']); // Navegar al componente RegistroComponent
-  }
-
-  navigateToHome() {
-    this.router.navigate(['/home']); // Navegar al componente HomeComponent
-  }
-
-  onSubmit() {
-    console.log(this.formlogin.value);
-    this.userService.loginUser(this.formlogin.value)
-        .then(response => {
-          console.log(response);
-          Swal.fire(
-            'User Login successfully',
-            '',
-            'success'
-          )
-          this.navigateToHome();
+    formlogin: FormGroup;
+    constructor(
+        private router: Router,
+        private userService: UserService
+    ) {
+        this.formlogin = new FormGroup({
+            email: new FormControl(),
+            password: new FormControl()
         })
-      .catch(error => {
-        console.log(error),
-          Swal.fire({
-            icon: 'error',
-            title: 'Oops...',
-            text: 'Datos erroneos'
-          })
-      });
-  }
+    }
 
-  onClick() {
-    this.userService.loginWithGoogle()
-        .then(response => {
-          console.log(response);
-          Swal.fire(
-            'User Login successfully',
-            '',
-            'success'
-          )
-          this.navigateToHome();
-        })
-      .catch(error => {
-        console.log(error),
-          Swal.fire({
-            icon: 'error',
-            title: 'Oops...',
-            text: 'Login fallo'
-          })
-      });
-  }
+    ngOnInit(): void {
+    }
+
+    navigateToRegistro() {
+        this.router.navigate(['/register']).then(r => {}); // Navegar al componente RegistroComponent
+    }
+
+    navigateToHome() {
+        this.router.navigate(['/home']).then(r => {}); // Navegar al componente HomeComponent
+    }
+
+    onSubmit() {
+        console.log(this.formlogin.value);
+        this.userService.loginUser(this.formlogin.value)
+            .then(response => {
+                console.log(response);
+                Swal.fire(
+                    'User Login successfully',
+                    '',
+                    'success'
+                )
+                this.navigateToHome();
+            })
+            .catch(error => {
+                console.log(error),
+                    Swal.fire({
+                        icon: 'error',
+                        title: 'Oops...',
+                        text: 'Datos erroneos'
+                    })
+            });
+    }
+
+    onClick() {
+        this.userService.loginWithGoogle()
+            .then(response => {
+                console.log(response);
+                Swal.fire(
+                    'User Login successfully',
+                    '',
+                    'success'
+                )
+                this.navigateToHome();
+            })
+            .catch(error => {
+                console.log(error),
+                    Swal.fire({
+                        icon: 'error',
+                        title: 'Oops...',
+                        text: 'Login fallo'
+                    })
+            });
+    }
 }
--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import {Router} from "@angular/router";
+import {UserService} from "../services/user.service";
+import {FormControl, FormGroup} from "@angular/forms";
+import Swal from "sweetalert2";
 
 @Component({
   selector: 'app-navbar',
@@ -6,5 +10,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent {
+  constructor(
+      private router: Router,
+      private userService: UserService
+  ) {
+  }
 
+  logOut() {
+    Swal.fire({
+      title: '¿Quieres cerrar sesion?',
+      showDenyButton: true,
+      confirmButtonText: 'Salir',
+      denyButtonText: `No salir`,
+    }).then((result) => {
+      if (result.isConfirmed) {
+        this.userService.logout().then(r => {})
+        this.router.navigate(['/login']).then(r => {});
+      }
+    })
+  }
 }
